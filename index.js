200a439f52bce9b1635c2519ab4dc69d334563d6
Doctorzik <ezekieljeremiah92@gmail>
2025-01-01 18:58:42 +0100

    Multer Image

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const multer = require("multer");
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
 
-
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+const upload = multer({ storage: storage });
 
 const port = process.env.PORT || 3000;
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  console.log(req.file, req.body);
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
+
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
